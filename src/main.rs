00005c39278b82f99871d49158ee3d46ff970ec1
rrokkam <rohithrokkam@gmail.com>
2020-07-02 22:04:56 -0700

    Clear screen and print each Termion key out as it comes in

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
-use std::io::{self, Read, Write};
+use std::io::{self, Write};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-fn main() {
-    let mut stdout = io::stdout().into_raw_mode().unwrap();
+#[derive(Default)]
+pub struct Editor {}
 
-    for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}\r", c);
-        if c == 'q' {
-            break;
+impl Editor {
+    pub fn run(&self) -> Result<(), std::io::Error> {
+        let _stdout = io::stdout().into_raw_mode()?;
+        self.clear_screen()?;
+
+        for key in io::stdin().keys() {
+            let done = self.on_keypress(key?);
+            if done {
+                self.clear_screen()?;
+                break;
+            }
         }
+
+        Ok(())
     }
-    writeln!(stdout, "Hello, world!").unwrap();
+
+    fn on_keypress(&self, key: Key) -> bool {
+        match key {
+            Key::Char(c) if c.is_control() => println!("{}\r", c),
+            Key::Char(c) => println!("{} ({})\r", c as u8, c),
+            Key::Ctrl('q') => return true,
+            _ => println!("{:?}\r", key),
+        }
+        false
+    }
+
+    fn clear_screen(&self) -> Result<(), std::io::Error> {
+        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        io::stdout().flush()
+    }
+}
+
+fn main() {
+    Editor::default().run().unwrap();
 }
