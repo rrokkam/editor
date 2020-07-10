07b834e4e6aafc07378de6bcb34c31fb5dfe6917
rrokkam <rohithrokkam@gmail.com>
2020-07-09 23:00:57 -0700

    Save state where cargo fmt errored out

--- src/editor.rs
+++ src/editor.rs
@@ -34,21 +34,30 @@ impl Editor {
     }
 
     pub fn run(&mut self) -> Result<(), std::io::Error> {
-        self.terminal.render(&self.buffer)?;
+        self.terminal.clear_screen();
         loop {
+            self.terminal.render(&self.buffer)?;
             let key = self.terminal.key().unwrap();
             match key {
                 Key::Char(c) if !c.is_control() => println!("{}\r", c),
-                Key::Up | Key::Down | Key::Left | Key::Right => self.move_cursor(key),
+                Key::Up => self.move_up(),
+                Key::Down => self.move_down(),
+                Key::Left => self.move_left(),
+                Key::Right => self.move_right(),
                 Key::Ctrl('s') => self.buffer.save(),
                 _ => break,
             }
-            self.terminal.refresh(&self.cursor_position)?;
         }
         self.terminal.clear_screen();
         Ok(())
     }
 
+    fn move_up(&mut self) {
+        self.cursor_position = Position {
+            row: pos.row.saturating_sub(1),
+            col: pos.col,
+        }
+    }
     fn move_cursor(&mut self, key: Key) {
         let pos = &self.cursor_position;
         self.cursor_position = match key {
@@ -69,6 +78,36 @@ impl Editor {
                 col: pos.col.saturating_add(1),
             },
             _ => *pos,
+        }
+    }
+    fn move_cursor(&mut self, key: Key) {
+        let pos = &self.cursor_position;
+        self.cursor_position = match key {
+            Key::Up => Position {
+                row: pos.row.saturating_sub(1),
+                col: pos.col,
+            },
+            Key::Down => Position {
+                row: pos.row.saturating_add(1),
+                col: pos.col,
+            },
+            Key::Left => Position {
+                row: pos.row,
+                col: pos.col.saturating_sub(1),
+            },
+            Key::Right => Position {
+                row: pos.row,
+                col: pos.col.saturating_add(1),
+            },
+            _ => *pos,
+        }
+    }
+
+    fn move_right(&mut self) {
+        self.cursor_position = Position {
+            row: pos.row,
+            col: pos.col.saturating_add(1),
+            }
         }
     }
 }
